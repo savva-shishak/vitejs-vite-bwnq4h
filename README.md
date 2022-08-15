c6de62cf6b37317529e46049fdee5bcca2889f8f
savva-shishak <s.shishak@yandex.ru>
2022-08-15 11:05:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-bwnq4h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-bwnq4h)
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return <div className="App">
+    <Routes>
+      <Route element={} />
+    </Routes>
+  </div>;
+}
+
+export default App;
